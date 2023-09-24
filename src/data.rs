0844b5847d7d0af3370e99b99da67756bee7f168
Nigam <nigam21nir@gmail.com>
2023-09-24 23:34:37 +0545

    Initial Commit

---
+++ src/data.rs
@@ -0,0 +1,8 @@
+pub struct battery_info{
+   pub battery_vendor : battery::units::ElectricCharge ,
+    
+}
+
+impl battery_info {
+   
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod data;
+
+fn main(){
+    pub struct units::electric_charge::abcoulomb;
+    let battery= data::battery_info{};
+    
+}
